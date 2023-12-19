3fb058d12645da6b0cd84ffdfc431e3aeea4db2a
Ilyasse <ilyassesalama7@gmail.com>
2023-12-19 02:20:52 +0100

    style: Improved directory listing HTML page

--- src/classes/response/HTMLGenerators.cpp
+++ src/classes/response/HTMLGenerators.cpp
@@ -1,7 +1,7 @@
 #include "../../../webserv.hpp"
 
 std::string generateHTMLStart(std::string path) {
-	return "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\"content=\"width=device-width, initial-scale=1.0\"><title>Document</title><link rel=\"preconnect\" href=\"https://fonts.googleapis.com\"><link rel=\"preconnect\" href=\"https://fonts.gstatic.com\"crossorigin><link href=\"https://fonts.googleapis.com/css2?family=Jost\"rel=\"stylesheet\"><style>html,body,p,ol,ul,li,dl,dt,dd,blockquote,figure,fieldset,legend,textarea,pre,iframe,hr,h1,h2,h3,h4,h5,h6{margin:0;padding:0}h1,h2,h3,h4,h5,h6{font-size:100%;font-weight:normal}ul{list-style:none}button,input,select{margin:0}html{box-sizing:border-box; font-family: \"Jost\", sans-serif;}*,*::before,*::after{box-sizing:inherit}img,video{height:auto;max-width:100%}iframe{border:0}table{border-collapse:collapse;border-spacing:0}td,th{padding:0}.auto_index_template {padding: 40px;width: 100%;}.path {font-size: 28px;}.line {width: 100%;height: 2px;background-color: #333;margin: 20px 0;}.files {display: flex;flex-direction: column;.file {display: grid;grid-template-columns: repeat(3, 1fr);justify-content: space-between;}.name {text-align: left;}.update {text-align: center;}.size {text-align: right;}</style></head><body><div class=\"auto_index_template\"><h1 class=\"path\">Index of " + path + "</h1><div class=\"line\"></div><div class=\"files\">";
+    return "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\"content=\"width=device-width, initial-scale=1.0\"><title>Document</title><link rel=\"preconnect\" href=\"https://fonts.googleapis.com\"><link rel=\"preconnect\" href=\"https://fonts.gstatic.com\"crossorigin><link href=\"https://fonts.googleapis.com/css2?family=Jost\"rel=\"stylesheet\"><style>html,body,p,ol,ul,li,dl,dt,dd,blockquote,figure,fieldset,legend,textarea,pre,iframe,hr,h1,h2,h3,h4,h5,h6{margin:0;padding:0}h1,h2,h3,h4,h5,h6{font-size:100%;font-weight:normal}ul{list-style:none}button,input,select{margin:0}html{box-sizing:border-box; font-family: \"Jost\", sans-serif;}*,*::before,*::after{box-sizing:inherit}img,video{height:auto;max-width:100%}iframe{border:0}table{border-collapse:collapse;border-spacing:0}td,th{padding:0}.auto_index_template {padding: 40px;width: 100%;}.path {font-size: 28px;}.line {width: 100%;height: 2px;background-color: #333;margin: 20px 0;}.files {display: flex;flex-direction: column;}.file {display: grid;grid-template-columns: repeat(3, 1fr);justify-content: space-between;}.header {font-weight: bold;margin-bottom: 10px;}.name, .update, .size {text-align: left;}</style></head><body><div class=\"auto_index_template\"><h1 class=\"path\">Index of " + path + "</h1><div class=\"line\"></div><div class=\"files\"><div class=\"file header\"><div class=\"name\">Name</div><div class=\"update\">Creation Date</div><div class=\"size\">Size</div></div>";
 }
 
 std::string createAnchor(std::string name) {
@@ -13,11 +13,11 @@ std::string createParagraph(std::string lastUpdate) {
 }
 
 std::string createSizeParagraph(long long size) {
-	return "<p class=\"size\">" + String::to_string(size) + "</p>";
+	return "<p class=\"size\">" + String::to_string(size) + " bytes</p>";
 }
 
 std::string createBodyHTML(std::string nameHTML, std::string lastUpdateHTML, std::string sizeHTML) {
-	return "<div class=\"file\">" + nameHTML + lastUpdateHTML + sizeHTML + "</div>";
+    return "<div class=\"file\">" + nameHTML + lastUpdateHTML + sizeHTML + "</div>";
 }
 
 std::string generateHTMLEnd(std::string bodyHTML) {
