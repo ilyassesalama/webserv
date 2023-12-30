bbc1151dd46e76e1ff71da73d83cc943d0dedac0
Abdellah Bahsine <69261429+AbdellahBahsine@users.noreply.github.com>
2023-12-30 11:55:20 +0100

    feat: Add the skeleton of how post response should be handled

--- src/classes/response/POSTResponseBuilder.cpp
+++ src/classes/response/POSTResponseBuilder.cpp
@@ -1,20 +1,41 @@
 #include "../../../webserv.hpp"
 
-void uploadSingleFile(std::map<std::string, std::string> headers, std::string &fileName, std::string body) {
-	fileName = File::generateFileName("uploaded") + File::getExtension(headers);
-	std::fstream myFile(File::getWorkingDir() + "/uploads/" + fileName, std::ios::binary | std::ios::app);
-	if (!myFile.is_open()) {
-		throw std::runtime_error("Error opening file");
-	}
-	myFile << body;
-}
+// void uploadSingleFile(std::map<std::string, std::string> headers, std::string &fileName, t_route *currentRoute) {
+
+// 	std::fstream bufferFile(fileName, std::ios::binary | std::ios::app);
+// 	if (!bufferFile.is_open()) {
+// 		throw Utils::WebservException("Error opening file");
+// 	}
+
+// 	std::fstream myFile(File::getWorkingDir() + currentRoute->upload_path + File::generateFileName("uploaded") + File::getExtension(headers), std::ios::binary | std::ios::app);
+// 	if (!myFile.is_open()) {
+// 		throw Utils::WebservException("Error opening file");
+// 	}
+// 	myFile << bufferFile.rdbuf();
+
+// 	myFile.close();
+// 	bufferFile.close();
+// }
 
 void Response::POSTResponseBuilder() {
-	this->isCGI = this->isLocationHasCGI();
-	if (!this->isCGI) {
-		uploadSingleFile(this->request->getHeaders(), this->request->getFileName(), this->request->getBody());
+
+	if (this->statusCode == 200) {
+		this->isCGI = this->isLocationHasCGI();
+
+		if (this->isCGI || !this->currentRoute->upload) {
+			this->setResponseBody();
+		} else {
+			this->statusCode = 201;
+			if (this->request->getIsRequestMultipart())
+				std::cout << "will enter if header is multipart" << std::endl;
+			else if (!this->request->getIsRequestChunked())
+				std::cout << "will enter if header is content-length" << std::endl;
+
+			this->responseBody = "";
+		}
 	}
-	this->responseBody = "";
+
+	
     this->setHeaders();
     this->setResponseLine();
 	this->fileOffset = -2;
