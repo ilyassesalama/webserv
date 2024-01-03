ce9d4b5f00034bac27b1b8bb4454eb50c46a4bd2
Badr Eddine El Kalai <badrkali1997@gmail.com>
2024-01-03 14:31:56 +0100

    fix : isPathAccessible on some edge cases

--- src/classes/request/RequestParserUtils.cpp
+++ src/classes/request/RequestParserUtils.cpp
@@ -64,8 +64,76 @@ void RequestParser::setServerInformation(t_server *server){
 
 // helper functions
 
-bool RequestParser::isPathAccessible() {
+//old Implementation
+// bool RequestParser::isPathAccessible() {
 
+//     this->requestResourcePath = "";
+//     size_t firstSlash = this->requestLine["path"].find_first_of("/");
+//     std::string location;
+//     bool slashRoute = false;
+
+//     if(firstSlash != std::string::npos) {
+//         size_t secondSlach = this->requestLine["path"].find_first_of("/", firstSlash + 1);
+//         if(secondSlach != std::string::npos) {
+//             location = this->requestLine["path"].substr(0, secondSlach);        
+//         }
+//         else location = "/";
+//     }
+//     t_route *route = NULL;
+//     t_route *slashR = NULL;
+//     for(std::vector<t_route>::iterator it = this->server->routes.begin(); it != this->server->routes.end(); ++it) {
+//         if(it->path == "/" && location != "/" && slashRoute == false) {
+//             slashRoute = true;
+//             slashR = &(*it);
+//         }
+//         if(location == it->path) {
+//             route = &(*it);
+//             break;
+//         }
+//     }
+//     if(route == NULL) {
+//         if(slashRoute == true) {
+//             this->route = slashR;
+//             this->requestResourcePath.append(File::getWorkingDir());
+//             this->requestResourcePath.append(slashR->root);
+//             this->requestResourcePath.append(getRequestLine()["path"]);
+//         }
+//         else return(false);
+
+//     } else {
+//         this->route = route;
+//         this->requestResourcePath.append(File::getWorkingDir());
+//         this->requestResourcePath.append(route->root);
+//         this->requestResourcePath.append(getRequestLine()["path"]);
+//     }
+//     if(FULL_LOGGING_ENABLED)
+//         Log::v("RequestParser: Request resource path after merge: " + this->requestResourcePath);
+//     return(File::isFile(this->requestResourcePath) || File::isDirectory(this->requestResourcePath));
+// }
+
+//new Implementation
+std::string normalizeURL(const std::string url) {
+    std::string normURL;
+    size_t i = 0;
+    size_t j = 0;
+
+    while (i < url.length()) {
+        normURL += url[i];
+        while (i < url.length() && url[i] == '/') {
+            i++;
+            if(url[i] != '/') {
+                i -= 1;
+                break;
+            }
+        }
+        j++;
+        i++;
+    }
+    return normURL;
+}
+bool RequestParser::isPathAccessible() {
+    this->requestLine["path"] = normalizeURL(this->requestLine["path"]);
+    std::cout << this->requestLine["path"] << std::endl;
     this->requestResourcePath = "";
     size_t firstSlash = this->requestLine["path"].find_first_of("/");
     std::string location;
@@ -103,7 +171,11 @@ bool RequestParser::isPathAccessible() {
         this->route = route;
         this->requestResourcePath.append(File::getWorkingDir());
         this->requestResourcePath.append(route->root);
-        this->requestResourcePath.append(getRequestLine()["path"]);
+        if(this->route->path != "/")
+            this->requestResourcePath.append(getRequestLine()["path"].substr(getRequestLine()["path"].find(location) + location.length()));
+        else
+            this->requestResourcePath.append(getRequestLine()["path"]);
+        
     }
     if(FULL_LOGGING_ENABLED)
         Log::v("RequestParser: Request resource path after merge: " + this->requestResourcePath);
@@ -246,3 +318,7 @@ void RequestParser::logParsedRequest(){
         Log::e("RequestParser: Parsing failed with code " + String::to_string(this->parsingState.statusCode) + " and reason: " + this->parsingState.statusMessage);
     }
 }
+
+// bool RequestParser::isRedirection() {
+
+// }
